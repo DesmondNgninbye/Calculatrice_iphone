68d0cfa2b009563748864bee801ffed1e0d74c67
DesmondNgninbye <desmonngninbye@gmail.com>
2024-12-21 17:53:52 +0100

    affichage a l'ecran terminer

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const buttons = document.getElementsByTagName("button")
-const song=document.getElementById("song")
+const song = document.getElementById("song")
+const numbers = document.getElementsByClassName("number")
+const operators = document .getElementsByClassName("operator")
+const screen = document .getElementById("screen")
+let result=""
+let lastIsOperator = false
 for(i=0;i<buttons.length;i++){
     buttons[i].addEventListener("click",playSong)
 
+}
+
+for(i=0;i<numbers.length;i++){
+    numbers[i].addEventListener("click",getNumber)
+
+}
+for(i=0;i<operators.length;i++){
+    operators[i].addEventListener("click",getoperator)
 }
 function playSong(){
     song.play()
     
 }
-console.log("song")
+function getNumber(event){
+    result= result+event.target.innerText
+    screen.innerText= result
+   
+}
+function getoperator(event){
+    if(lastIsOperator==false){
+    result= result+event.target.innerText
+    screen.innerText= result
+    lastIsOperator= true
+}
+}
+console.log(screen)
